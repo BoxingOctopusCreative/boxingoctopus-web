17549753a100d3927a3ea889342a3456752ea628
Ryan Draga <ryan.draga@boxingoctop.us>
2022-02-10 18:35:52 -0500

    Add Content to Project Page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class Projects extends Component {
     render() {
         return (
-            <Row>
-                <Col>
-                    <div className='text-splash'>This is the Projects Page</div>
-                </Col>
-            </Row>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className='text-splash'>Projects</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <img src="https://ryandraga.me/content/images/2021/06/Banner-Logo-2.png" width="400px" alt="ryandraga.me"/>
+                    </Col>
+                    <Col>
+                        <p>
+                            My Personal Blog
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <img src="https://mylifeinmusic.me/content/images/size/w600/2021/05/MLiM-Title.png" width="400px" alt="mylifeinmusic.me"/>
+                    </Col>
+                    <Col>
+                        <p>
+                            My Life In Music
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         );
     };
 };
