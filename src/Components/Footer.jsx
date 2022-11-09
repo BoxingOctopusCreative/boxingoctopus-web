38cc63d631ee0a93bbe15db6530fe203355e2f4d
Ryan Draga <ryan.draga@boxingoctop.us>
2022-11-09 14:52:42 -0500

    Fix Mastodon Verification

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import Parser from 'html-react-parser';
 
 const BoxingOctopusUrl = '<a href="https://boxingoctop.us" target="_blank" rel="noreferrer">Boxing Octopus Creative</a>'
 const ReactUrl         = '<a href="https://reactjs.org" target="_blank" rel="noreferrer">React</a>'
+const MastodonUrl      = '<a rel="me" href="https://mastodon.social/@boxingoctopus">Mastodon</a>'
 
 const date =  new Date().getFullYear();
 
@@ -10,9 +11,8 @@ export const Footer = () => (
   <div className="fixed-bottom">
     <footer className="page-footer font-small pt4">
       <div className="footer-copyright text-center py-3">
-        Another fine {Parser(BoxingOctopusUrl)} Project | Built with {Parser(ReactUrl)} | Copyright © {date} {Parser(BoxingOctopusUrl)}
+        Another fine {Parser(BoxingOctopusUrl)} Project | Built with {Parser(ReactUrl)} | Copyright © {date} {Parser(BoxingOctopusUrl)} | Mastodon Verification: {Parser(MastodonUrl)}
       </div>
-      <a rel="me" href="https://mastodon.social/@boxingoctopus">Mastodon</a>
     </footer>
   </div>
 );
